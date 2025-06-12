004b5bed049223e5f4bcd8f8d3f0b27a71a4927c
Rahiman Shaik <rahimanshaik13@gmail.com>
2025-06-12 16:43:48 +0530

    mongo

--- config/db.js
+++ config/db.js
@@ -7,9 +7,11 @@ dotenv.config(); // Load environment variables
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
-      socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-      family: 4 // Use IPv4, skip trying IPv6
+      // serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+      // socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
+      // family: 4 // Use IPv4, skip trying IPv6
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
